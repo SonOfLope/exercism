95920ea4942cc69d271314b9b191d4e9ae39fb80
jonathanL <jonathanlopez@hotmail.ca>
2021-01-11 12:05:17 -0500

    Exercise completed ✔

--- bob/bob.go
+++ bob/bob.go
@@ -1,15 +1,39 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+//Package bob provides conversational answers
 package bob
 
-// Hey should have a comment documenting it.
+import "strings"
+
+//Hey will make conversation following normal rules regarding sentence punctuation in English.
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+
+	remark = strings.TrimSpace(remark)
+	var lastWord string
+	if len(remark) == 0 {
+		lastWord = ""
+	} else {
+		lastWord = remark[len(remark)-1:]
+	}
+
+	if lastWord == "?" {
+		if strings.ToUpper(remark) == remark && isLetter(remark) {
+			return "Calm down, I know what I'm doing!"
+		} else {
+			return "Sure."
+		}
+	} else if strings.ToUpper(remark) == remark && isLetter(remark) {
+		return "Whoa, chill out!"
+	} else if remark == "" {
+		return "Fine. Be that way!"
+	} else {
+		return "Whatever."
+	}
+}
+
+func isLetter(s string) bool {
+	for _, r := range s {
+		if ('a' <= r && r <= 'z') || ('A' <= r && r <= 'Z') {
+			return true
+		}
+	}
+	return false
 }
--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -1,1 +1,61 @@
 package tree
+
+import (
+	"errors"
+	"sort"
+)
+
+// Record represents the parent child relationship of records
+type Record struct {
+	ID     int
+	Parent int
+}
+
+// Node represents the list of child records of each record
+type Node struct {
+	ID       int
+	Children []*Node
+}
+
+// Build builds a tree from the list of parent child relationship of records
+func Build(records []Record) (*Node, error) {
+
+	if len(records) == 0 {
+		return nil, nil
+	}
+
+	sort.Slice(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
+
+	recMap := make(map[int]*Node)
+
+	for i, r := range records {
+
+		// check for id continuity
+		if r.ID != i {
+			return nil, errors.New("Non continous record id")
+		}
+
+		if _, ok := recMap[r.ID]; ok {
+			return nil, errors.New("Duplications are not allowed")
+		}
+
+		if r.ID != 0 && r.ID <= r.Parent {
+			return nil, errors.New("Invalid ID number")
+		}
+
+		if r.ID == 0 && r.Parent != 0 {
+			return nil, errors.New("Root node cannot have a parent")
+		}
+
+		n := &Node{ID: r.ID}
+		recMap[r.ID] = n
+
+		if _, ok := recMap[r.Parent]; ok && r.ID != 0 {
+			recMap[r.Parent].Children = append(recMap[r.Parent].Children, n)
+		}
+	}
+
+	return recMap[0], nil
+}
