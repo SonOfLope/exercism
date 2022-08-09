2a91ef1f5624e336738d19f8c12365402002cd88
SonOfLope <jonathanlopez@hotmail.ca>
2022-08-08 20:15:36 -0400

    completed parsing-log-files :heavy_check_mark:

---
+++ go/parsing-log-files/parsing_log_files.go
@@ -0,0 +1,44 @@
+package parsinglogfiles
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func IsValidLine(text string) bool {
+	re := regexp.MustCompile(`^\[(TRC|DBG|INF|WRN|ERR|FTL)\]`)
+	return re.MatchString(text)
+}
+
+func SplitLogLine(text string) []string {
+	re := regexp.MustCompile(`<[-=~*]*>`)
+	return re.Split(text, -1)
+}
+
+func CountQuotedPasswords(lines []string) int {
+	re := regexp.MustCompile(`(?i)".*password.*"`)
+	count := 0
+	for _, str := range lines {
+		if re.MatchString(str) {
+			count++
+		}
+	}
+	return count
+}
+
+func RemoveEndOfLineText(text string) string {
+	re := regexp.MustCompile(`end-of-line\d*`)
+	return re.ReplaceAllString(text, "")
+}
+
+func TagWithUserName(lines []string) []string {
+	re := regexp.MustCompile(`User\s+(\w+)`)
+	for i, line := range lines {
+		founds := re.FindStringSubmatch(line)
+		if founds != nil {
+			line = fmt.Sprintf("[USR] %s %s", founds[1], line)
+		}
+		lines[i] = line
+	}
+	return lines
+}
